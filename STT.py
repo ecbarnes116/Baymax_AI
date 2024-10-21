044387b4a820d7c6867526b58ff0762c3e33b65c
Ethan <ethanbarnes316@gmail.com>
2024-10-20 20:54:31 -0500

    audio transcriber functional

--- STT.py
+++ STT.py
@@ -3,43 +3,68 @@ import config
 from pvrecorder import PvRecorder
 import wave
 import struct
-import datetime
+from datetime import datetime
+import os
 
-for index, device in enumerate(PvRecorder.get_audio_devices()):
-    print(f"[{index}] {device}")
-
-def STT(audio_file):
-    aai.settings.api_key = config.assemblyai_key
-    transcriber = aai.Transcriber()
-
-    # transcript = transcriber.transcribe("https://assembly.ai/news.mp4")
-    transcript = transcriber.transcribe("./my-local-audio-file.wav")
-
-    print(transcript.text)
-
-    return transcript.text
+if not os.path.exists("audio_files"):
+    os.mkdir("audio_files")
+if not os.path.exists("transcript_files"):
+    os.mkdir("transcript_files")
 
 
 
 def record_audio():
-    date_time = datetime.now().strftime("%Y-%m-%d %H;%M;%S")
-    path = f"audio_files/{date_time}.wav"
+    date_time = datetime.now().strftime("%Y-%m-%d_%H;%M;%S")
+
+    audio_file = date_time
+    audio_file_path = f"audio_files/{audio_file}.wav"
 
     recorder = PvRecorder(device_index=-1, frame_length=512)
     audio = []
 
     try:
+        print("Sarting recoring...")
         recorder.start()
 
         while True:
             frame = recorder.read()
             audio.extend(frame)
+
     except KeyboardInterrupt:
         recorder.stop()
-        with wave.open(path, 'w') as f:
+        print("Recording ended")
+
+        with wave.open(audio_file_path, 'w') as f:
             f.setparams((1, 2, 16000, 512, "NONE", "NONE"))
             f.writeframes(struct.pack("h" * len(audio), *audio))
+
     finally:
         recorder.delete()
+    
+    return audio_file
 
+
+
+def STT(audio_file, save=True):
+    aai.settings.api_key = config.ASSEMBLYAI_KEY
+    transcriber = aai.Transcriber()
+
+    audio_file_path = f"audio_files/{audio_file}.wav"
+
+    transcript = transcriber.transcribe(audio_file_path)
+
+    print(transcript.text)
+
+    if save:
+        transcript_path = f"transcript_files/{audio_file}.txt"
+
+        with open(transcript_path, 'w') as f:
+            f.write(transcript.text)
+
+    return transcript.text
+
+
+
+audio_file = record_audio()
+trancript = STT(audio_file=audio_file)
 
